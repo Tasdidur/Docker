6604fd84e3649777a7aaee0d3b552a2afdeba6c9
Tasdidur <tasdid@appscode.com>
2021-09-03 15:41:56 +0600

    added

---
+++ API_Server_GO_Chi/Server.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/jwtauth/v5"
+)
+
+var (
+	adminToken *jwtauth.JWTAuth
+	userToken  *jwtauth.JWTAuth
+)
+
+func init() {
+	adminToken = jwtauth.New("HS256", []byte("papel"), []byte("papel"))
+	userToken = jwtauth.New("HS256", []byte("manana"), []byte("manana"))
+}
+
+func main() {
+	r := chi.NewRouter()
+
+	fmt.Println("server running...")
+
+	r.Post("/createUser", createUser)
+	r.Post("/loginAsAdmin", loginAdmin)
+	r.Post("/loginAsUser", loginUser)
+	r.Post("/logout", func(w http.ResponseWriter, r *http.Request) {
+		http.SetCookie(w, &http.Cookie{
+			Name:  "jwt",
+			Value: "",
+		})
+		http.Redirect(w, r, ":8081/", 200)
+	})
+
+	r.Route("/admin", admin)
+	r.Route("/user", user)
+
+	log.Fatal(http.ListenAndServe(":8081", r))
+}
+
+func admin(r chi.Router) {
+
+	r.Use(jwtauth.Verifier(adminToken))
+	r.Use(jwtauth.Authenticator)
+
+	r.Route("/products", func(r chi.Router) {
+		r.Get("/", showAll)
+		r.Post("/", addNewProduct)
+		r.Put("/{c}", updateProduct) // c here means quantity
+		r.Delete("/", deleteProduct)
+	})
+}
+
+func user(r chi.Router) {
+
+	r.Use(jwtauth.Verifier(userToken))
+	r.Use(jwtauth.Authenticator)
+
+	r.Get("/showInvoice", getInvoice)
+	r.Route("/product", func(r chi.Router) {
+		r.Get("/search", searchProduct)
+		r.Post("/buy", buyProduct)
+
+	})
+}
---
+++ API_Server_GO_Chi/go.mod
@@ -0,0 +1,8 @@
+module API_Server_GO_Chi
+
+go 1.16
+
+require (
+	github.com/go-chi/chi/v5 v5.0.3
+	github.com/go-chi/jwtauth/v5 v5.0.1
+)
